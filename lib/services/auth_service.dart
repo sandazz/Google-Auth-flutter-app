6d50ec843c9d48c61de7798c2aee1351558297f0
sandazz <sandazzzg1999@gmail.com>
2024-05-27 22:14:07 +0530

    Add Screen shots

--- lib/services/auth_service.dart
+++ lib/services/auth_service.dart
@@ -5,22 +5,18 @@ class AuthService {
   // Google Sign in
   signInWithGoogle() async {
     // Begin interactive sign in process
-    print("signin1");
     final GoogleSignInAccount? gUser = await GoogleSignIn().signIn();
 
     // obtain auth details from request
-    print("signin2");
     final GoogleSignInAuthentication gAuth = await gUser!.authentication;
 
     // create a new credential for user
-    print("signin3");
     final credential = GoogleAuthProvider.credential(
       accessToken: gAuth.accessToken,
       idToken: gAuth.idToken
     );
 
     // finally, lets sign in
-    print("signin4");
     return await FirebaseAuth.instance.signInWithCredential(credential);
   }
 }
